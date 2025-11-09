7ed791f6f4f493704db1a96e47a476fbf781cfe0
Taoshif1 <taoshif2@gmail.com>
2025-11-10 02:33:55 +0600

    Client site created & neccessary packages installed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div class="navbar bg-base-100 shadow-sm">
+        <a class="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+    </>
+  );
+}
+
+export default App;
